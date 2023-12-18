e6061e27aff852ed68133673fa28db4f526aa7b4
eabdikhalyk <eabdikhalyk@mail.ru>
2023-12-18 10:54:42 +0600

    added test checkboxes

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,9 @@
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class BasePage:
-    def __init__(self,driver,url):
+    def __init__(self, driver, url):
         self.driver = driver
         self.url = url
 
@@ -24,10 +25,8 @@ class BasePage:
     def element_is_not_visible(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
 
-
     def element_is_clickable(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
 
     def go_to_element(self, element):
-        self.driver.execute_script('argument[0].scrollIntoView();', element)
-
+        self.driver.execute_script('arguments[0].scrollIntoView();', element)
