b0fa243e6b2afdf04d886501e6aa87c67709d53f
eabdikhalyk <eabdikhalyk@mail.ru>
2023-12-18 10:54:42 +0600

    added radio button check

---
+++ locators/elements_page_locators.py
@@ -0,0 +1,29 @@
+from selenium.webdriver.common.by import By
+
+class TextBoxPageLocators:
+    # Form fields
+    FULL_NAME = (By.CSS_SELECTOR,"input[id='userName']")
+    EMAIL = (By.CSS_SELECTOR,"input[id='userEmail']")
+    CURRENT_ADDRESS = (By.CSS_SELECTOR,"textarea[id='currentAddress']")
+    PERMANENT_ADDRESS = (By.CSS_SELECTOR,"textarea[id='permanentAddress']")
+    SUBMIT = (By.CSS_SELECTOR,"button[id='submit']")
+    # Created form
+    CREATED_FULL_NAME = (By.CSS_SELECTOR,"p[id='name']")
+    CREATED_EMAIL = (By.CSS_SELECTOR,"p[id='email']")
+    CREATED_CURRENT_ADDRESS = (By.CSS_SELECTOR,"p[id='currentAddress']")
+    CREATED_PERMANENT_ADDRESS = (By.CSS_SELECTOR,"p[id='permanentAddress']")
+
+class CheckBoxPageLocators:
+    EXPAND_ALL_BUTTON = (By.CSS_SELECTOR,"button[title='Expand all']")
+    ITEM_LIST = (By.CSS_SELECTOR,"span[class='rct-title']")
+    CHECKED_ITEMS = (By.CSS_SELECTOR,"svg[class='rct-icon rct-icon-check']")
+    TITLE_ITEM = (".//ancestor::span[@class='rct-text']")
+    OUTPUT_RESULT = (By.CSS_SELECTOR,"span[class='text-success']")
+
+class RadioButtonLocators:
+    # RADIOS = (By.CSS_SELECTOR,"label[class='custom-control-label']")
+    # OUTPUT_RESULT = (By.CSS_SELECTOR,"span[class='text-success']")
+    YES_RADIO = (By.CSS_SELECTOR,"label[for='yesRadio']")
+    IMPRESSIVE_RADIO = (By.CSS_SELECTOR,"label[for='impressiveRadio']")
+    NO_RADIO = (By.CSS_SELECTOR,"label[for='noRadio']")
+    OUTPUT_RESULT = (By.CSS_SELECTOR,"p span[class='text-success']")
